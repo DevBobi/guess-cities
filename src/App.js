3204a39a34b8713ee9285a2be875579952fff339
devbobi <md.saifurrahaman.cse@gmail.com>
2022-06-01 16:30:40 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import { useState } from 'react';
 import './App.css';
+import Init from './components/Init';
 import Map from './components/Map';
 
 
 function App() {
-  // const [start, setStart] = useState(false)
+  const [start, setStart] = useState(false)
   
   return (
     <div className="App shadow-lg border-2">
-      {/* {start ? <Map /> : <Init start={start} setStart={setStart} />} */}
-      <Map />
+      {start ? <Map /> : <Init start={start} setStart={setStart} />}
     </div>
   );
 }
